5489c2c209876207a65b011ddcb3cc9920d8b799
MeGustas <32269100@qq.com>
2025-11-14 20:40:25 +0800

    update Configurations

--- powermoniter/settings.py
+++ powermoniter/settings.py
@@ -246,6 +246,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://81.68.79.234",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
